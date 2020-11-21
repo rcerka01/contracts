54776464e1b61e47890746c0bcdcfb426861d676
Ray <raitis.cerkasovs@gmail.com>
2020-11-21 15:43:22 +0000

    lottery, pickWiner

--- random/Lottery.sol
+++ random/Lottery.sol
@@ -1,6 +1,27 @@
 pragma solidity ^0.4.17;
 
-contract Inbox {
+contract Lottery {
 
+    address public manager;
+    address[] public players;
     
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        
+        players.push(msg.sender);
+    }
+    
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWiner() public {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+    }
 }
+
